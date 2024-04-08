7ac9e1a13a9124793c10f56725d6f993c80d1dbd
Moussia <Moussia@Moussias-MacBook.local>
2024-04-08 15:57:30 -0400

    main checkstyles

--- src/main/java/adry/fallingsand/Main.java
+++ src/main/java/adry/fallingsand/Main.java
@@ -2,15 +2,6 @@ package adry.fallingsand;
 
 public class Main {
     public static void main(String[] args) {
-//        Sand sand = new Sand(50, 10);
-//        sand.randomSand(50);
-//        System.out.println(sand.toString());
-//        Scanner scanner = new Scanner(System.in);
-//        String kb = scanner.nextLine();
-//        while (kb.isEmpty()) {
-//            sand.fall();
-//            System.out.println(sand.toString());
-//            kb = scanner.nextLine();}
         new SandFrame().setVisible(true);
     }
 }
